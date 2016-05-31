1b7b2de4307c7c9fe56a06c53976d61d99c0cb07
Yasic <2207793765@qq.com>
2016-05-31 11:18:13 +0800

    Add the drawable files

--- app/src/main/java/com/yasic/diycode/MainActivity.java
+++ app/src/main/java/com/yasic/diycode/MainActivity.java
@@ -37,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public int sum(int a, int b){
+        return a + b;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
